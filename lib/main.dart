1a4807e76955a1c33612d5950cd84afca86529a9
Denys <dentef767@gmail.com>
2023-10-26 13:47:17 +0300

    26.10.2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test_cnn/screens/home_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
